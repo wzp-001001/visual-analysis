5fa4575987c1db0a3420f910394838abc7cbcdcf
zhangzq0521 <779377069@qq.com>
2023-11-30 20:55:58 +0800

    更新app

--- server/app.py
+++ server/app.py
@@ -2,6 +2,16 @@ import uuid
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from markupsafe import Markup
+# from service.baidu_migration_scale import BaiduMigrationScale
+# from jinja2 import Markup
+# from jinja2.utils import markupsafe
+
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+
+
+app = Flask(__name__, static_folder="templates")
 
 
 BOOKS = [
@@ -81,6 +91,30 @@ def single_book(book_id):
         remove_book(book_id)
         response_object['message'] = 'Book removed!'
     return jsonify(response_object)
+
+
+@app.route('/baidu/baidu_migration_scale', methods=['GET'])
+def all_baidu_migration_scales():
+    response_object = {'status': 'success'}
+    # baiduMigrationScale = BaiduMigrationScale(db_file="../db/migration_scale_index.db")
+    # result_dict = baiduMigrationScale.listAllBaiduMigrationScales(baiduMigrationScale.connection)
+    # response_object['data'] = result_dict
+    return jsonify(response_object)
+
+def bar_base() -> Bar:
+    c = (
+        Bar()
+            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+            .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+    )
+    return c
+
+@app.route("/bar")
+def bar_view():
+    c = bar_base()
+    return Markup(c.render_embed())
 
 
 if __name__ == '__main__':
