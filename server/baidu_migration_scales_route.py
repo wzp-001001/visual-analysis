79cf2d3e2a683593db7f47d5db6f25c2cf1110ab
zhangzq0521 <779377069@qq.com>
2023-12-03 23:15:17 +0800

    折线图

--- server/baidu_migration_scales_route.py
+++ server/baidu_migration_scales_route.py
@@ -1,14 +1,45 @@
 from flask import jsonify
 from service.baidu_migration_scale import BaiduMigrationScale
-
+import pyecharts.options as opts
+from pyecharts.charts import Line
+from markupsafe import Markup
 
 def register_routes(app):
 
+    def line_base(x_data,y_data_1,y_data_2,y_data_3) -> Line:
+        return (
+            Line()
+            .add_xaxis(xaxis_data=x_data)
+            .add_yaxis(
+                series_name="2021年",
+                y_axis=y_data_1,
+                label_opts=opts.LabelOpts(is_show=False),
+            )
+            .add_yaxis(
+                series_name="2022年",
+                y_axis=y_data_2,
+                label_opts=opts.LabelOpts(is_show=False),
+            )
+            .add_yaxis(
+                series_name="2023年",
+                y_axis=y_data_3,
+                label_opts=opts.LabelOpts(is_show=False),
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title="黄金周前后迁徙趋势(万人)"),
+                tooltip_opts=opts.TooltipOpts(trigger="axis"),
+                yaxis_opts=opts.AxisOpts(
+                    type_="value",
+                    axistick_opts=opts.AxisTickOpts(is_show=True),
+                    splitline_opts=opts.SplitLineOpts(is_show=True),
+                ),
+                xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
+            )
+        )
+
     @app.route('/baidu/baidu_migration_scale', methods=['GET'])
     def all_baidu_migration_scales():
-        response_object = {'status': 'success'}
         baiduMigrationScale = BaiduMigrationScale(db_file="./db/migration_scale_index.db")
-        print(baiduMigrationScale)
         result_dict = baiduMigrationScale.listAllBaiduMigrationScales(baiduMigrationScale.connection)
-        response_object['data'] = result_dict
-        return jsonify(response_object)
+        c = line_base(result_dict['month_day'],result_dict['2021'],result_dict['2022'],result_dict['2023'])
+        return Markup(c.render_embed())
--- server/service/baidu_migration_scale.py
+++ server/service/baidu_migration_scale.py
@@ -15,18 +15,23 @@ class BaiduMigrationScale(DbImpl):
                     where date between 20230901 and 20231015
                     or date between 20220901 and 20221015
                     or date between 20210901 and 20211015
+                    order by date
                   '''
             cur = conn.cursor()
             cur.execute(sql)
             result_set = cur.fetchall()
+            month_day = []
             for result in result_set:
                 date_int = result[0]
                 year = str(date_int)[0:4]
+                if year == '2023':
+                    month_day.append(str(date_int)[4:])
                 if year in result_dict:
                     # 如果这个键不在字典中，创建一个新的列表并添加这个元组（去掉第三个元素）
-                    result_dict[year].append((result[0], result[1]))
+                    result_dict[year].append((result[1]))
                 else:
-                    result_dict[year] = [(result[0], result[1])]
+                    result_dict[year] = [(result[1])]
+            result_dict['month_day']=month_day
         return result_dict
 
 # baiduMigrationScale = BaiduMigrationScale(db_file="../db/migration_scale_index.db")
