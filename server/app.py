ee33191d8741ec2712fcc90b54d05e2cf68eda56
zhangzq0521 <779377069@qq.com>
2023-12-06 14:33:46 +0800

    前端静态页面

--- server/app.py
+++ server/app.py
@@ -1,12 +1,12 @@
 import uuid
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template,send_from_directory
 from flask_cors import CORS
 from markupsafe import Markup
 from baidu_migration_scales_route import register_routes
 from bei_shang_guang_shen_baidu_route import register_routes2
 from national_baidu_migration_after import register_routes_national_baidu
-
+import os
 
 from pyecharts import options as opts
 from pyecharts.charts import Bar
@@ -15,42 +15,12 @@ from pyecharts.charts import Bar
 app = Flask(__name__, static_folder="templates")
 
 
-BOOKS = [
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
-
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
-
-
-def remove_book(book_id):
-    for book in BOOKS:
-        if book['id'] == book_id:
-            BOOKS.remove(book)
-            return True
-    return False
-
 register_routes(app)
 register_routes2(app)
 register_routes_national_baidu(app)
@@ -59,45 +29,6 @@ register_routes_national_baidu(app)
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
-
-
-@app.route('/books', methods=['GET', 'POST'])
-def all_books():
-    response_object = {'status': 'success'}
-    if request.method == 'POST':
-        post_data = request.get_json()
-        BOOKS.append({
-            'id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read')
-        })
-        response_object['message'] = 'Book added!'
-    else:
-        response_object['books'] = BOOKS
-    return jsonify(response_object)
-
-
-@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
-def single_book(book_id):
-    response_object = {'status': 'success'}
-    if request.method == 'PUT':
-        post_data = request.get_json()
-        remove_book(book_id)
-        BOOKS.append({
-            'id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read')
-        })
-        response_object['message'] = 'Book updated!'
-    if request.method == 'DELETE':
-        remove_book(book_id)
-        response_object['message'] = 'Book removed!'
-    return jsonify(response_object)
-
-
-
 
 def bar_base() -> Bar:
     c = (
@@ -114,7 +45,12 @@ def bar_view():
     c = bar_base()
     return Markup(c.render_embed())
 
-
+@app.route('/')
+def home():
+    return render_template('line_chart.html')
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('static', filename)
 if __name__ == '__main__':
     # app.run(debug=True)
     app.run()
